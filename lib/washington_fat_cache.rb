ba09fdcc3b3e33ac313029ff186ed613da605d98
phinze <paul.t.hinze@gmail.com>
2010-03-01 16:18:16 -0600

    BDD'ed through initial spec/implementation

--- lib/washington_fat_cache.rb
+++ lib/washington_fat_cache.rb
@@ -1,18 +1,21 @@
 require 'rubygems'
 require 'ruby-debug'
 class WashingtonFatCache
-  @@fetchers = {}
-  @@fatcache = {}
+
   class << self
+    @initted = false
+    attr_accessor :fetchers, :fatcache, :indexed_fatcache
 
     def store(*key_and_maybe_data, &fetcher)
+      init unless initted? # for first time store
+
       if key_and_maybe_data.length == 2 && fetcher.nil?
         key, data = key_and_maybe_data
-        @@fatcache[key] = data
+        fatcache[key] = data
       elsif key_and_maybe_data.length == 1 && fetcher
         key = key_and_maybe_data.first
-        @@fetchers[key] = fetcher
-        @@fatcache[key] = fetcher.call
+        fetchers[key] = fetcher
+        fatcache[key] = fetcher.call
       else
         argstr   = "#{key_and_maybe_data.length} arguments"
         blockstr = (block_given?) ? 'a block' : 'no block'
@@ -21,17 +24,99 @@ class WashingtonFatCache
     end
 
     def get(key)
-      raise "no data for #{key}" unless @@fatcache.key?(key)
-      @@fatcache[key]
+      unless cached?(key)
+        if fetchable?(key)
+          fatcache[key] = fetchers[key].call
+        else
+          raise "no data for #{key}" 
+        end
+      end
+      fatcache[key]
+    end
+
+    def lookup(key, options={})
+      options = options.dup
+      by      = [*options.delete(:by)]
+      using   = [*options.delete(:using)]
+      
+      # create index if it doesn't exist
+      index(key, by) unless indexed?(key, by)
+     
+      return indexed_fatcache[key][by][using]
+    end
+
+    def index(key, on)
+      # must have cache data to work with
+      ensure_cached(key)
+
+      # ensure we're dealing with an array, we're such a friendly API!
+      on = [*on]
+
+      # init hash if we've never indexed for this key before
+      indexed_fatcache[key] = {} unless indexed_fatcache.has_key?(key)
+
+      raw_data = get(key)
+      
+      # calls each method specified in the `on` array once on each element in
+      # the raw dataset, and uses the results of those calls to key this index
+      indexed_fatcache[key][on] = raw_data.group_by { |x| on.map { |b| x.send(b) } }
+    end
+
+    def get_index(key, on)
+      on = [*on]
+
+      ensure_indexed(key, on)
+
+      indexed_fatcache[key][on]
+    end
+
+    def ensure_cached(key)
+      raise "no data for #{key}" unless cached?(key)
+    end
+
+    def ensure_indexed(key, on)
+      ensure_cached(key)
+      raise "no index for #{key} on #{on.inspect}" unless indexed?(key, on)
+    end
+
+    def cached?(key)
+      fatcache && fatcache.has_key?(key)
+    end
+
+    def indexed?(key, on)
+      indexed_fatcache                   && 
+      indexed_fatcache.has_key?(key)     &&
+      indexed_fatcache[key].has_key?(on)
+    end
+
+    def fetchable?(key)
+      fetchers && fetchers.has_key?(key)
     end
 
     def invalidate(key)
-      @@fatcache.delete(key)
+      init unless initted? 
+      
+      fatcache.delete(key)
     end
 
     def reset!
-      @@fetchers = {}
-      @@fatcache = {}
+      self.fetchers         = nil
+      self.fatcache         = nil
+      self.indexed_fatcache = nil
+      @initted              = nil
+    end
+
+    protected
+
+    def init
+      self.fetchers         = {}
+      self.fatcache         = {}
+      self.indexed_fatcache = {}
+      @initted             = true
+    end
+
+    def initted?
+      @initted == true
     end
   
   end
--- spec/washington_fat_cache_spec.rb
+++ spec/washington_fat_cache_spec.rb
@@ -1,66 +1,145 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe WashingtonFatCache do
-  after { WashingtonFatCache.reset! }
+  # purely for brevity in the specs
+  before { @fatcache = WashingtonFatCache }
+
+  # so we don't have leaky state
+  after  { WashingtonFatCache.reset! }
 
   describe 'get(key)' do
-
     it 'raises an exception if no data is stored for this key' do
       lambda { 
-        WashingtonFatCache.get(:not_there) 
+        @fatcache.get(:not_there) 
       }.should raise_error(/not_there/)
     end
 
+    describe 'when data has been invalidated for a key with a fetcher' do
+      it 'uses a fetcher to get new data' do
+        i = 0
+        @fatcache.store(:increment) { i += 1 }
+        @fatcache.invalidate(:increment)
+        @fatcache.get(:increment).should == 2
+      end
+    end
   end
 
   describe 'store(key[, data])' do
-
     describe 'when called with key and data arguments' do
       it 'stores data for specified key' do
-        WashingtonFatCache.store(:five_alive, 5)
-        WashingtonFatCache.get(:five_alive).should == 5
+        @fatcache.store(:five_alive, 5)
+        @fatcache.get(:five_alive).should == 5
       end
 
       it 'properly stores nil for key if explicitly specified' do
-        WashingtonFatCache.store(:empty_inside, nil)
-        WashingtonFatCache.get(:empty_inside).should be_nil
+        @fatcache.store(:empty_inside, nil)
+        @fatcache.get(:empty_inside).should be_nil
       end
     end
 
     describe 'when called with key and fetcher block' do
       it 'uses block as fetcher to retrieve data to store' do
-        WashingtonFatCache.store(:fetched_from_block) { 'cheese sandwich' }
-        WashingtonFatCache.get(:fetched_from_block).should == 'cheese sandwich'
+        @fatcache.store(:fetched_from_block) { 'cheese sandwich' }
+        @fatcache.get(:fetched_from_block).should == 'cheese sandwich'
+      end
+
+      it 'calls the block only once, caching the data returned' do
+        i = 0
+        @fatcache.store(:increment) { i += 1 }
+
+        first_result = @fatcache.get(:increment)
+        second_result = @fatcache.get(:increment)
+
+        first_result.should == 1
+        second_result.should == 1
       end
     end
-
   end
 
-  describe 'lookup' do
+  describe 'lookup(key, :by => [:method_names], :using => [:index_key])' do
+    it 'returns a records stored in the dataset specified by key, indexed by the specified methods, and with the following key to the index' do
+      @fatcache.store(:a_set, [0,1,2,3,4,5])
+      result = @fatcache.lookup(:a_set, :by => :odd?, :using => true)
+      result.should == [1,3,5]
+    end
+
+    it 'works with multi-element index keys' do
+      @fatcache.store(:a_set, [0,1,2,3,4,5])
+      result = @fatcache.lookup(:a_set, :by => [:even?, :zero?], :using => [true, true])
+      result.should == [0]
+    end
   end
 
-  describe 'index' do
+  describe 'get_index(key, on)' do
+    it 'returns the given index for a key' do
+      @fatcache.store(:numbers, [0,1,2,3,4])
+      @fatcache.index(:numbers, :odd?)
+      @fatcache.get_index(:numbers, :odd?).should be_a Hash
+    end
+
+    it 'raises an error if no index exists for on specified key' do
+      @fatcache.store(:indexed_one_way, [123])
+      @fatcache.index(:indexed_one_way, :zero?)
+      lambda {
+        @fatcache.get_index(:indexed_one_way, :odd?)
+      }.should raise_error(/indexed_one_way.*odd?/)
+    end
+  end
+
+  describe 'index(key, on)' do
+    it 'raises an error if there is no raw data to index for specified key' do
+      lambda {
+        @fatcache.index(:wishbone, :whats_the_story?)
+      }.should raise_error(/wishbone/)
+    end
+
+    it 'raises an error if the elements of the dataset do not respond to the index key methods' do
+      @fatcache.store(:numbers, [1,2,3,4,5,6])
+      lambda {
+        @fatcache.index(:numbers, :millionaire?)
+      }.should raise_error(/millionaire?/)
+    end
+    
+    describe 'given a dataset which responds to methods' do
+      before do
+        fruit = [
+          stub(:mango,  :grams_of_awesome => 3),
+          stub(:banana, :grams_of_awesome => 10),
+          stub(:apple,  :grams_of_awesome => 3)
+        ]
+        @fatcache.store(:fruit, fruit)
+      end
+      it 'calls each method specified in `on` array and uses the results as the index key' do
+        @fatcache.index(:fruit, [:grams_of_awesome])
+        index = @fatcache.get_index(:fruit, :grams_of_awesome)
+        index.keys.should =~ [[3],[10]]
+      end
+    end
   end
 
   describe 'invalidate(key)' do
-
     describe 'when no fetcher has been specified for key' do
       it 'returns the last value the cache had for the key' do
-        WashingtonFatCache.store(:once_upon_a_time, 33)  
-        retval = WashingtonFatCache.invalidate(:once_upon_a_time)
+        @fatcache.store(:once_upon_a_time, 33)  
+        retval = @fatcache.invalidate(:once_upon_a_time)
         retval.should == 33
       end
 
       it 'removes data stored for a given key' do
-        WashingtonFatCache.store(:there_and_gone, 100)  
-        WashingtonFatCache.invalidate(:there_and_gone)
+        @fatcache.store(:there_and_gone, 100)  
+        @fatcache.invalidate(:there_and_gone)
         lambda {
-          WashingtonFatCache.get(:there_and_gone)
+          @fatcache.get(:there_and_gone)
         }.should raise_error(/there_and_gone/)
       end
     end
 
     describe 'when a fetcher has been specified for a key' do
+      it 'does not clear out the fetcher, which can be used in the next lookup' do
+        @fatcache.store(:fetch_me) { "I've been fetched" }
+        @fatcache.invalidate(:fetch_me)
+        @fatcache.get(:fetch_me).should == "I've been fetched" 
+      end
     end
   end
 end
